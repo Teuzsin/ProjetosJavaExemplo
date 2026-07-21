ae00ad184e1aee76db129b3d7b7ab992697b536f
TeuSziz <teuszinvest@gmail.com>
2026-07-20 21:07:29 -0300

    Update Commit

--- src/aboutMe.java
+++ src/aboutMe.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// TeuSziz☄ 20/07/2026
+// Estudo deo controle de fluxo (Try e Catch)
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+import java.util.InputMismatchException; // exceção relatada pela IDE
+import java.util.Locale; // importa a biblioteca
+import java.util.Scanner; // importa a biblioteca do scanner
+
+public class aboutMe{
+
+    public static void main(String[] Args){
+        try { // Inicia o bloco de Try Catch
+
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US); //define as funções de conversão do scanner
+
+            System.out.println("Digite seu nome:"); // recebe o nome
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome:"); // recebe o sobrenome
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade:"); // recebe a idade
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura:"); // recebe a altura
+            double altura = scanner.nextDouble();
+
+            // imprime as informções recebidas pelo scanner
+
+            System.out.println("Olá me chamo" + " " + nome + " " + sobrenome);
+            System.out.println(" ");
+            System.out.println("Minha idade é" + " " + idade);
+            System.out.println(" ");
+            System.out.println("Minha altura é" + " " + altura + "Cm");
+            System.out.println(" ");
+            System.out.println("Essas são minhas informações");
         }
+            catch (InputMismatchException e){ // executa o comando para a exceção
+                System.out.println("Os campos idade e altura, devem ser preenchidos de forma numérica");
+                System.out.println("Por favor, insira novamente os valores corretamente");
+            }
     }
 }
